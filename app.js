56026b25b1e816b7f5fd47467ab881e9273ea094
[Elia <[elitoribio01@gmail.com]>
2022-06-24 21:36:22 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const saveBtn = document.getElementById("save-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads(){
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+    `
+    }
+    ulEl.innerHTML = listItems
+}
+
